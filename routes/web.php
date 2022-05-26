c271122a89aad5bbd3f29b384264287879188fbf
EnnyZakiyyah <enny.9h08@gmail.com>
2022-05-26 08:09:18 +0700

    Init new Navigasi

--- routes/web.php
+++ routes/web.php
@@ -14,43 +14,65 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home.home');
+    return view('home.home', [
+        "title" => "Perpustakaan | LPTP Surakarta",
+    ]);
 });
 
 //Home-Sirkulasi 
 Route::get('/home/sirkulasi/peminjaman-buku', function () {
-    return view('home.sirkulasi.peminjaman-buku');
+    return view('home.sirkulasi.peminjaman-buku', [
+        "title" => "Perpustakaan | Sirkulasi",
+    ]);
 });
 Route::get('/home/sirkulasi/pengembalian-buku', function () {
-    return view('home.sirkulasi.pengembalian-buku');
+    return view('home.sirkulasi.pengembalian-buku', [
+        "title" => "Perpustakaan | Sirkulasi",
+    ]);
 });
 Route::get('/home/sirkulasi/penelusuran-katalog', function () {
-    return view('home.sirkulasi.penelusuran-katalog');
+    return view('home.sirkulasi.penelusuran-katalog', [
+        "title" => "Perpustakaan | Sirkulasi",
+    ]);
 });
 Route::get('/home/sirkulasi/penelusuran-katalog/detil', function () {
-    return view('home.detil.detil-penelusuran-katalog');
+    return view('home.detil.detil-penelusuran-katalog', [
+        "title" => "Perpustakaan | Sirkulasi",
+    ]);
 });
 Route::get('/home/sirkulasi/bebas-pustaka', function () {
-    return view('home.sirkulasi.bebas-pustaka');
+    return view('home.sirkulasi.bebas-pustaka', [
+        "title" => "Perpustakaan | Sirkulasi",
+    ]);
 });
 
 //Home-Layanan
 Route::get('/home/layanan/keanggotaan', function () {
-    return view('home.layanan.keanggotaan');
+    return view('home.layanan.keanggotaan', [
+        "title" => "Perpustakaan | Layanan",
+    ]);
 });
 Route::get('/home/layanan/bibliography', function () {
-    return view('home.layanan.bibliography');
+    return view('home.layanan.bibliography', [
+        "title" => "Perpustakaan | Layanan",
+    ]);
 });
 Route::get('/home/layanan/bibliography/detil', function () {
-    return view('home.detil.detil-bibliography');
+    return view('home.detil.detil-bibliography', [
+        "title" => "Perpustakaan | Layanan",
+    ]);
 });
 
 //Home-Koleksi-Digital
 Route::get('/home/koleksi-digital/koleksi-digital', function () {
-    return view('home.koleksi-digital');
+    return view('home.koleksi-digital', [
+        "title" => "Perpustakaan | Koleksi Digital",
+    ]);
 });
 Route::get('/home/koleksi-digital/koleksi-digital/detil', function () {
-    return view('home.detil-koleksi-digital');
+    return view('home.detil-koleksi-digital', [
+        "title" => "Perpustakaan | Koleksi Digital",
+    ]);
 });
 
 // Route::get('/home/koleksi-digital/booklet', function () {
@@ -74,15 +96,21 @@ Route::get('/home/koleksi-digital/koleksi-digital/detil', function () {
 
 //Auth
 Route::get('/sign-in', function () {
-    return view('auth/sign-in');
+    return view('auth/sign-in', [
+        "title" => "Perpustakaan | Sign In",
+    ]);
 });
 
 Route::get('/sign-up', function () {
-    return view('auth/sign-up');
+    return view('auth/sign-up', [
+        "title" => "Perpustakaan | Sign Up",
+    ]);
 });
 
 
 //Dashboard
 Route::get('/dashboard', function () {
-    return view('dashboard.dashboard');
+    return view('dashboard.dashboard', [
+        "title" => "Perpustakaan | Dashboard",
+    ]);
 });
